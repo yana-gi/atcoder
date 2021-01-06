ca96566251eb1a82fc3d4b3b80c7ab9a53f0650c
yana-gi <yanagisawa.hika@gmail.com>
2021-01-06 19:05:07 +0900

    abc183 ab

--- abc184/b/main.rb
+++ abc184/b/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+n, x = gets.split.map(&:to_i)
+r = gets.chomp.split("").map do |i|
+  if i == 'o'
+    x += 1
+  else
+    x -= 1 unless x.zero?
+  end
+end
+puts x
