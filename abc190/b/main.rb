cc850996018cb927de607f19e335b24202ea3c00
yana-gi <yanagisawa.hika@gmail.com>
2021-04-23 14:16:11 +0900

    abc190 b

--- abc190/b/main.rb
+++ abc190/b/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+jumon_num, non_time, non_damage = gets.split.map(&:to_i)
+jumon_ary = jumon_num.times.map { gets.split.map(&:to_i) }
+jumon_ary.each do |jumon|
+  if (non_time > jumon[0] && non_damage < jumon[1])
+    puts 'Yes'
+    return
+  end
+end
+puts 'No'
