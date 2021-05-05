f064f33dfad271375a4d96bf130a41c3a1e4356c
yana-gi <yanagisawa.hika@gmail.com>
2021-05-05 10:47:08 +0900

    abc192 b

--- abc192/b/main.rb
+++ abc192/b/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+input = gets.chomp
+odd = []
+even = []
+input.each do |i|
+  odd << i if i.odd?
+  even << i if i.even?
+end
+
+odd.join('').downcase == odd.join('') && odd.join('').upcase == odd.join('')
