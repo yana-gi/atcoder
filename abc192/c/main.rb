dc41d804650f61ac39443d7ada28371f871dd9d1
yana-gi <yanagisawa.hika@gmail.com>
2021-05-07 11:05:40 +0900

    abc192 c

--- abc192/c/main.rb
+++ abc192/c/main.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+
+n, k = gets.split(' ').map(&:to_i)
+
+def g1(x)
+  x.to_s.split('').map(&:to_i).sort.reverse.join.to_i
+end
+
+def g2(x)
+  x.to_s.split('').map(&:to_i).sort.join.to_i
+end
+
+def func(x)
+  g1(x) - g2(x)
+end
+
+num = n
+k.times { num = func(num.to_s) }
+p num
+
